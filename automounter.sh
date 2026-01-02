81b1b809a98b36101c119e65c4fc3fcd858ef540
Alessandra Ioana Spataroaia <alessandraioanaspataroaia@gmail.com>
2026-01-02 18:23:00 +0200

    adding automatic unmounting when exiting the program

--- automounter.sh
+++ automounter.sh
@@ -15,7 +15,7 @@ fi
 
 declare -A MNT_MAP
 declare -A LAST_USED # se retine timestamp ul ultimei utilizari pt fiecare mountpoint
-TTL=300 # time to live e max de 5 minute
+TTL=15 # time to live e max de 5 minute
 
 while read -r folder device; do
 
@@ -23,11 +23,8 @@ while read -r folder device; do
         continue
     fi
 
-    MNT_MAP["$folder"]="$device"
-    # if [[ -z "$folder" || -z "$device" ]]; then
-    #     echo "Eroare! Folderul sau fisierul nu exista."
-    #     exit 1
-    # fi
+    MNT_MAP[$(realpath -m "$folder")]="$device"
+
 done < "$CONFIG_FILE"
 
 echo "Configurarea este gata!"
@@ -43,23 +40,27 @@ echo "Pornire AutomounterShell (amsh)..."
 
 #functie care verifica daca ttl a expirat 
 #si care verifica mountpoint urile si le demonteaza
-cleaup_mounts(){ 
+cleanup_mounts(){ 
     now=$(date +%s) #timp curent 
     for mp in "${!LAST_USED[@]}";do #parcurgem toate mountpoint urile
         if mountpoint -q "$mp"; then # verificam daca mountpoint ul este montat
             last=${LAST_USED[$mp]} #retinem timpul ultimei utilizari
             if (( now - last > TTL )); then 
-                echo "TTL expirat pt $mp -> umount"
-                sudo umount "$mp" #demontam automat
-                unset LAST_USED["$mp"] #stergere intrare din last_used
+                if [[ "$PWD" == "$mp"* ]]; then
+                    LAST_USED["$mp"]=$now
+                else
+                    echo "TTL expirat pt $mp -> umount"
+                    sudo umount "$mp" #demontam automat
+                    unset LAST_USED["$mp"] #stergere intrare din last_used
+                fi
             fi
         fi
     done }
 
 while true; do
-    cleaup_mounts #cerificam existenta mountpoint urilor carora le a expirat ttl ul
+    cleanup_mounts #verificam existenta mountpoint urilor carora le a expirat ttl ul
     # afiseaza prompt-ul personalizat "amsh>"
-    read -p "amsh> " command_line
+    read -p "amsh:${PWD##/}> " command_line # ${PWD##/} arata si directorul curent
 
     # elimina spatiile albe de la inceput/sfarsit
     command_line=$(echo "$command_line" | xargs)
@@ -73,18 +74,24 @@ while true; do
     read -r cmd arg1 <<< "$command_line"
 
     if [[ "$cmd" == "exit" ]]; then
-        echo "Iesire din amsh."
-        break 
+        echo "Iesire din amsh. Se demonteaza automat resursele active..."
+        for mp in "${!LAST_USED[@]}"; do
+            if mountpoint -q "$mp"; then 
+                sudo umount "$mp" 
+            fi 
+        done
+        exit 0
     
     elif [[ "$cmd" == "cd" ]]; then
-        TARGET_DIR="$arg1"
 
         # daca utilizatorul a tastat doar 'cd' fara argumente, mergi in directorul home
-        if [[ -z "$TARGET_DIR" ]]; then
+        if [[ -z "$arg1" ]]; then
+            TARGET_DIR="$HOME"
             cd ~
             continue
         fi
-
+        
+        TARGET_DIR=$(realpath -m "$arg1")
         MOUNT_DEVICE=${MNT_MAP["$TARGET_DIR"]}
 
         if [[ -n "$MOUNT_DEVICE" ]]; then
@@ -95,27 +102,31 @@ while true; do
             # va trebui sa introduci parola de administrator cand rulezi amsh
             if ! mountpoint -q "$TARGET_DIR"; then
                 echo "Dispozitivul nu este montat. Se monteaza acum..."
+                echo "S-a montat $MOUNT_DEVICE in $TARGET_DIR "
                 sudo mount "$MOUNT_DEVICE" "$TARGET_DIR"
-                if [[ $? -eq 0 ]]; then
-                    echo "Montare reusita! Schimb directorul."
-                    cd "$TARGET_DIR"
-                    LAST_USED["$TARGET_DIR"]=$(date +%s) #salvam timpul ultimei utilizari
-                else
-                    echo "Eroare la montarea dispozitivului. Verifica permisiunile."
-                fi
             else
                 echo "Dispozitivul este deja montat. Schimb directorul."
-                cd "$TARGET_DIR"
-                LAST_USED["$TARGET_DIR"]=$(date +%s) #actualizare timpul ult utilizari
             fi
 
+            if [[ $? -eq 0 ]]; then
+                cd "$TARGET_DIR" || echo "Eroare la montarea dispozitivului. Verifica permisiunile."
+                echo "Montare reusita! Schimb directorul."
+                LAST_USED["$TARGET_DIR"]=$(date +%s) #salvam timpul ultimei utilizari
+            fi
         else
             echo "Comanda 'cd' normala catre: $TARGET_DIR"
             cd "$TARGET_DIR"
         fi
-
     else
-        $command_line
+        eval "$command_line"
+
+        for mp in "{!MNT_MAP[@]}"; do
+            if [[ "$command_line" == *"$mp"* ]]; then
+                if mountpoint -q "$mp"; then 
+                    LAST_USED["$mp"]=$(date + %s)
+                fi
+            fi
+        done
     fi
 done
 
